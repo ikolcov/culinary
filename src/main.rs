8b89407fd6881c64888d70a44de57442cdc1a0cb
Ivan Koltsov <ikolcov@yandex.ru>
2023-08-16 11:18:10 +0300

    wip

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use anyhow::Context;
+use clap::Parser;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let config = culinary::config::Config::parse();
+
+    let db = sqlx::postgres::PgPoolOptions::new()
+        .max_connections(50)
+        .connect(&config.database_url)
+        .await
+        .context("could not connect to database_url")?;
+
+    sqlx::migrate!().run(&db).await?;
+
+    Ok(())
+}
